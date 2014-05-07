3d017f402f8e5da16eaa81ab04971f60d4e755ef
James Kiesel <james.kiesel@gmail.com>
2014-05-06 23:06:58 -0400

    Update CD link

--- db/seeds.rb
+++ db/seeds.rb
@@ -84,7 +84,7 @@ Social.create! [
   { name: 'Twitter', href: 'http://www.twitter.com/SidetrackedProductions', title: 'Follow us on Twitter', icon: 'twitter', on_contact: true },
   { name: 'Youtube', href: 'http://www.youtube.com/YourSidetracked', title: 'Watch us on Youtube', icon: 'youtube', on_contact: true },
   { name: 'Email', href: "mailto:#{email.value}", icon: 'envelope'},
-  { name: 'Buy our CD!', href: 'http://bit.do/SidetrackedCD', icon: 'music', on_contact: true },
+  { name: 'Buy our CD!', href: 'http://Kunaki.com/Sales.asp?PID=PX00Z6JZOG', icon: 'music', on_contact: true },
   { name: 'View our Demo', href: 'http://www.youtube.com/watch?v=3YhBU5dKOso', icon: 'film' }
 ]
 puts 'Socials created'
